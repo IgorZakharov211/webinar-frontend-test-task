42ce98ed592e4dcda8359f359ac2df4e3ceeccc0
Ïgor Zakharov <Igor1994294@yandex.ru>
2021-11-09 17:01:06 +0300

    fxi: reset

--- src/TodoItems.tsx
+++ src/TodoItems.tsx
@@ -1,147 +1,157 @@
-import { useCallback } from 'react';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Checkbox from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import DeleteIcon from '@material-ui/icons/Delete';
-import { makeStyles } from '@material-ui/core/styles';
-import classnames from 'classnames';
-import { motion } from 'framer-motion';
-import { TodoItem, useTodoItems } from './TodoItemsContext';
-import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
-import { ContactSupportOutlined } from '@material-ui/icons';
+import { useCallback } from "react";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { makeStyles } from "@material-ui/core/styles";
+import classnames from "classnames";
+import { motion } from "framer-motion";
+import { TodoItem, useTodoItems } from "./TodoItemsContext";
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from "react-beautiful-dnd";
+import { ContactSupportOutlined } from "@material-ui/icons";
 
 const spring = {
-    type: 'spring',
-    damping: 25,
-    stiffness: 120,
-    duration: 0.25,
+  type: "spring",
+  damping: 25,
+  stiffness: 120,
+  duration: 0.25,
 };
 
 const useTodoItemListStyles = makeStyles({
-    root: {
-        listStyle: 'none',
-        padding: 0,
-    },
+  root: {
+    listStyle: "none",
+    padding: 0,
+  },
 });
 
-
 export const TodoItemsList = function () {
-    const { todoItems } = useTodoItems();
+  const { todoItems } = useTodoItems();
 
-    const classes = useTodoItemListStyles();
+  const classes = useTodoItemListStyles();
 
-    let sortedItems = todoItems.slice().sort((a, b) => {
-        if (a.done && !b.done) {
-            return 1;
-        }
-
-        if (!a.done && b.done) {
-            return -1;
-        }
-
-        return 0;
-    });
-
-    const handleOnDragEnd = (result: DropResult) => {
-        if(!result.destination) return;
-        const items = Array.from(todoItems);
-        const [reorderedItem] = items.splice(result.source.index, 1);
-        items.splice(result.destination.index, 0, reorderedItem);
-        
-        localStorage.setItem('todoListState', JSON.stringify({"todoItems": items}))
-        sortedItems = items;
+  let sortedItems = todoItems.slice().sort((a, b) => {
+    if (a.done && !b.done) {
+      return 1;
     }
 
+    if (!a.done && b.done) {
+      return -1;
+    }
 
+    return 0;
+  });
 
-    return (
-        <DragDropContext onDragEnd={handleOnDragEnd}>
-            <Droppable droppableId="todolist">
-                {(provided) =>(
-                <ul className={classes.root} {...provided.droppableProps} ref={provided.innerRef}>
-                    {sortedItems.map((item, index) => (
-                        <Draggable key={item.id} draggableId={item.id} index={index}>
-                            {(provided) => (
-                                <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                                <TodoItemCard item={item} />
-                                </li>
-                            )}
-                        </Draggable>  
-                    ))}
-                    {provided.placeholder}
-                </ul>
+  const handleOnDragEnd = (result: DropResult) => {
+    if (!result.destination) return;
+    const items = Array.from(todoItems);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    localStorage.setItem("todoListState", JSON.stringify({ todoItems: items }));
+    sortedItems = items;
+  };
+
+  return (
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="todolist">
+        {(provided) => (
+          <ul
+            className={classes.root}
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {sortedItems.map((item, index) => (
+              <Draggable key={item.id} draggableId={item.id} index={index}>
+                {(provided) => (
+                  <li
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                  >
+                    <TodoItemCard item={item} />
+                  </li>
                 )}
-            </Droppable>
-        </DragDropContext>
-    );
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </ul>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
 };
 
 const useTodoItemCardStyles = makeStyles({
-    root: {
-        marginTop: 24,
-        marginBottom: 24,
-    },
-    doneRoot: {
-        textDecoration: 'line-through',
-        color: '#888888',
-    },
+  root: {
+    marginTop: 24,
+    marginBottom: 24,
+  },
+  doneRoot: {
+    textDecoration: "line-through",
+    color: "#888888",
+  },
 });
 
 export const TodoItemCard = function ({ item }: { item: TodoItem }) {
-    const classes = useTodoItemCardStyles();
-    const { dispatch } = useTodoItems();
+  const classes = useTodoItemCardStyles();
+  const { dispatch } = useTodoItems();
 
-    const handleDelete = useCallback(
-        () => dispatch({ type: 'delete', data: { id: item.id } }),
-        [item.id, dispatch],
-    );
+  const handleDelete = useCallback(
+    () => dispatch({ type: "delete", data: { id: item.id } }),
+    [item.id, dispatch]
+  );
 
-    const handleToggleDone = useCallback(
-        () =>
-            dispatch({
-                type: 'toggleDone',
-                data: { id: item.id },
-            }),
-        [item.id, dispatch],
-    );
+  const handleToggleDone = useCallback(
+    () =>
+      dispatch({
+        type: "toggleDone",
+        data: { id: item.id },
+      }),
+    [item.id, dispatch]
+  );
 
-    return (
-        <Card
-            className={classnames(classes.root, {
-                [classes.doneRoot]: item.done,
-            })}
-        >
-            <CardHeader
-                action={
-                    <IconButton aria-label="delete" onClick={handleDelete}>
-                        <DeleteIcon />
-                    </IconButton>
-                }
-                title={
-                    <FormControlLabel
-                        control={
-                            <Checkbox
-                                checked={item.done}
-                                onChange={handleToggleDone}
-                                name={`checked-${item.id}`}
-                                color="primary"
-                            />
-                        }
-                        label={item.title}
-                    />
-                }
-            />
-            {item.details ? (
-                <CardContent>
-                    <Typography variant="body2" component="p">
-                        {item.details}
-                    </Typography>
-                </CardContent>
-            ) : null}
-        </Card>   
-    );
+  return (
+    <Card
+      className={classnames(classes.root, {
+        [classes.doneRoot]: item.done,
+      })}
+    >
+      <CardHeader
+        action={
+          <IconButton aria-label="delete" onClick={handleDelete}>
+            <DeleteIcon />
+          </IconButton>
+        }
+        title={
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={item.done}
+                onChange={handleToggleDone}
+                name={`checked-${item.id}`}
+                color="primary"
+              />
+            }
+            label={item.title}
+          />
+        }
+      />
+      {item.details ? (
+        <CardContent>
+          <Typography variant="body2" component="p">
+            {item.details}
+          </Typography>
+        </CardContent>
+      ) : null}
+    </Card>
+  );
 };
